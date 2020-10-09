ac1019fc78a62d1b40dcf60d256fcd09dd291ea9
RodrigoLima26 <rodrigoscarpimlima@hotmail.com>
2020-10-09 15:39:43 -0300

    Http client

--- endpoints/activity-service.js
+++ endpoints/activity-service.js
@@ -1,10 +1,11 @@
 import { ENV } from '@app/env';
+import { HttpClient } from '@angular/common/http';
 
 export class ActivityService {
 
     getActivities() {
         return new Promise((resolve, reject) => {
-            this.http.get(`${ENV.appUrl}ramos-atividade`)
+            HttpClient.get(`${ENV.appUrl}ramos-atividade`)
                 .subscribe(
                     (data = {data: {}}) => resolve(data),
                            (err) => reject(err)
