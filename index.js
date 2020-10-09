bb652abf7d116ce2687ce0b7ed58cc1bdc7c8cee
RodrigoLima26 <rodrigoscarpimlima@hotmail.com>
2020-10-09 16:07:23 -0300

    change to .ts

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import {ActivityService} from "./endpoints/activity-service";
+import {ActivityService} from "./endpoints/activity-service.ts";
 
 export var F5EndPoints = {
     activity: ActivityService
